51a372040025beba7cf2bde7c2766e1e0d22ebf2
SparksFlash <ug2102056@cse.pstu.ac.bd>
2026-01-25 23:31:50 +0600

    Class

---
+++ sourav_practice/OOP/class_object.dart
@@ -0,0 +1,50 @@
+import 'human1.dart';
+
+main() {
+  ///without class
+  String StudentName = 'Rahim';
+  String StudentClass = 'Class-10';
+  String Studentaddress = 'Dhaka';
+
+  String StudentName2 = 'Karim';
+  String StudentClass2 = 'Class-10';
+  String Studentaddress2 = 'Dhaka';
+
+  //with class
+
+  ///create an object
+
+  Student student1 = Student();
+  student1.studentName = 'Rahim';
+  student1.studentClass = 'class-10';
+  student1.studentAddress = 'Dhaka';
+
+  print(student1.studentName);
+  print(student1.studentClass);
+
+  ///Object created
+  ///
+  ///
+
+  // print(Human1().classname1());
+  // Human1().StaticMethod1();
+
+  Human1 maruf = Human1();
+  print(maruf.legs);
+  print(maruf.eyes);
+  print(maruf.hands);
+  maruf.moving();
+
+  Human1 selim = Human1();
+  selim.eyes = 1;
+  selim.hands = 1;
+  print(selim.legs);
+  print(selim.eyes);
+  print(selim.hands);
+}
+
+class Student {
+  String? studentName;
+  String? studentClass;
+  String? studentAddress;
+}
---
+++ sourav_practice/OOP/human1.dart
@@ -0,0 +1,19 @@
+class Human1 {
+  int legs = 2;
+  int hands = 2;
+  int eyes = 2;
+  String? name;
+  static String classname1 = 'Human Class';
+
+  static StaticMethod1() {
+    print('Static Method');
+  }
+
+  moving() {
+    print('Human is moving');
+  }
+
+  method1() {
+    print('Human is monitoring');
+  }
+}
